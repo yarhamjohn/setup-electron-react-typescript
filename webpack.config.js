ba59d2b012e580417e8726a05b91275931bbfc39
John Yarham <yarhamjohn@gmail.com>
2020-12-10 21:45:34 +0000

    Add react webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,38 @@ const electronConfiguration = {
   }
 }
 
+const reactConfiguration = {
+    mode: 'development',
+    entry: './src/renderer.tsx',
+    target: 'electron-renderer',
+    devtool: 'source-map',
+    resolve: {
+      alias: {
+        ['@']: path.resolve(__dirname, 'src')
+      },
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts(x?)$/,
+          include: /src/,
+          use: [{ loader: 'ts-loader' }]
+        }
+      ]
+    },
+    output: {
+      path: __dirname + '/dist',
+      filename: 'renderer.js'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html'
+      })
+    ]
+  }
+
 module.exports = [
   electronConfiguration,
+  reactConfiguration
 ];
